f56f9d186b680bb8f8580186e14c9f999346f084
Randhal Ramirez <randhalramirez@gmail.com>
2022-04-13 21:56:52 -0600

    initial simple site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Pdf from './pdf/news.pdf';
+import Pdf2 from './pdf/cape_report.pdf';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a id={'newsPdf'} href={Pdf} target={'_blank'} rel="noreferrer">{'Go to news PDF'}</a>
+        <br/>
+      <a id={'capePdf'} href={Pdf2} target={'_blank'} rel="noreferrer">{'Go to Cape. PDF'}</a>
     </div>
   );
 }
